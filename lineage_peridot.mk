575c5ad9c3412d20adf2dad8fb8106d220771ad4
droidcore <agusmahardika99@gmail.com>
2025-11-21 01:26:03 +0700

    fix

---
+++ lineage_peridot.mk
@@ -0,0 +1,44 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit from peridot device
+$(call inherit-product, device/xiaomi/peridot/device.mk)
+
+PRODUCT_NAME := lineage_peridot
+PRODUCT_DEVICE := peridot
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_BRAND := POCO
+PRODUCT_MODEL := 24069PC21G
+
+PRODUCT_SYSTEM_NAME := peridot_global
+PRODUCT_SYSTEM_DEVICE := peridot
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildFingerprint=POCO/peridot_global/peridot:15/AQ3A.240912.001/OS2.0.206.0.VNPMIXM:user/release-keys \
+    DeviceName=$(PRODUCT_SYSTEM_DEVICE) \
+    DeviceProduct=$(PRODUCT_SYSTEM_NAME)
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Flags
+TARGET_BUILD_APERTURE_CAMERA := false
+TARGET_DISABLE_EPPE := true
+# Evox
+WITH_GMS := true
+TARGET_OPTIMIZED_DEXOPT := true
+TARGET_ENABLE_BLUR := true
+TARGET_HAS_UDFPS := true
+EXTRA_UDFPS_ANIMATIONS := true
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
---
+++ vendorsetup.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+# Vendor (fresh clone)
+echo "Cloning vendor tree..."
+rm -rf vendor/xiaomi/peridot
+git clone https://github.com/sm8635-dev/vendor_xiaomi_peridot.git vendor/xiaomi/peridot
+
+# Kernel source (fresh clone)
+echo "Cloning kernel source tree..."
+rm -rf kernel/xiaomi/sm8635
+git clone -b lineage-23.0 --depth 1 https://github.com/droidcore/android_kernel_xiaomi_sm8635.git kernel/xiaomi/sm8635
+rm -rf kernel/xiaomi/sm8635-modules
+git clone -b lineage-23.0 --depth 1 https://github.com/droidcore/android_kernel_xiaomi_sm8635-modules.git kernel/xiaomi/sm8635-modules
+
+rm -rf kernel/xiaomi/sm8635-devicetrees
+git clone -b lineage-23.0 --depth 1 https://github.com/peridot-dev/android_kernel_xiaomi_sm8635-devicetrees.git kernel/xiaomi/sm8635-devicetrees
+
+# Hardware xiaomi (fresh clone)
+echo "Cloning hardware xiaomi source..."
+rm -rf hardware/xiaomi
+git clone -b lineage-23.0 https://github.com/sm8635-dev/hardware_xiaomi.git hardware/xiaomi
+
+# MiuiCamera device tree (fresh clone)
+echo "Cloning MiuiCamera device tree..."
+rm -rf device/xiaomi/peridot-miuicamera
+git clone https://github.com/F6-test/device_xiaomi_peridot-miuicamera.git device/xiaomi/peridot-miuicamera
+
+# MiuiCamera vendor tree (fresh clone)
+echo "Cloning MiuiCamera vendor tree..."
+rm -rf vendor/xiaomi/peridot-miuicamera
+git clone https://github.com/F6-test/vendor-xiaomi-peridot-miuicamera.git vendor/xiaomi/peridot-miuicamera
+
+# Viper4Android 
+echo "Cloning Viper4Android tree..."
+rm -rf packages/apps/ViPER4AndroidFX
+git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX.git packages/apps/ViPER4AndroidFX
+
+# KProfiles (fresh clone)
+echo "Cloning KProfiles..."
+rm -rf packages/apps/KProfiles
+git clone -b lineage-23.0 https://github.com/sm8635-dev/packages_apps_KProfiles.git packages/apps/KProfiles
+
+# FastCharge (fresh clone)
+echo "Cloning FastCharge..."
+rm -rf packages/apps/FastCharge
+git clone -b lineage-23.0 https://github.com/sm8635-dev/packages_apps_FastCharge.git packages/apps/FastCharge
+
+# Compat (cherry pick)
+echo "Fetching QPR1 compat..."
+cd hardware/lineage/compat
+git fetch https://github.com/stx-staging/android_hardware_statix_compat bp3a
+git reset --hard FETCH_HEAD
+
+# Picking sepolicy for QPR1 (cherry pick)
+echo "Picking sepolicy fix..."
+cd ../../..
+cd device/qcom/sepolicy_vndr/sm8650
+git fetch https://github.com/sm8635-dev/device_qcom_sepolicy_vndr
+git cherry-pick 39cfd17977cc664fa8393b6569c39179f4127b2d 1d2c884133bb23d780fc35ecff27d2e6eeabe314 8e148a4417233704f40c223c0624d41f017b490e
+
+# Refresh signing keys
+if [ -d vendor/lineage-priv/keys ]; then
+  echo "Removing existing signing keys..."
+  rm -rf vendor/evox/keys
+fi
+echo "Cloning fresh signing keys..."
+git clone https://github.com/droidcore/priv-key.git -b main vendor/lineage-priv/keys
+
+# Always back to root at the end
+if command -v croot &>/dev/null; then
+  croot
+else
+  cd "$ANDROID_BUILD_TOP" || true
+fi
+
+echo "vendorsetup.sh execution complete."
